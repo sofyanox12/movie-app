d10c5f213ad19fa7cbf529e76e3c551b506647aa
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-06-08 21:26:10 +0800

    setting up the basics

--- app/src/main/java/com/sisfo/practicumfinale/activities/MediaActivity.java
+++ app/src/main/java/com/sisfo/practicumfinale/activities/MediaActivity.java
@@ -1,14 +1,16 @@
-package com.sisfo.practicumfinale;
+package com.sisfo.practicumfinale.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.sisfo.practicumfinale.R;
+
+public class MediaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_media);
     }
 }
