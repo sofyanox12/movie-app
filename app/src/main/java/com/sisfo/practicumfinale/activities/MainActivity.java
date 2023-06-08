0cb9af4fd67ec7cfb6d490cad01fc31b63cba081
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-06-09 02:03:46 +0800

    commit: remove `framelayout`
    we are going to mainpulate the recycler view

--- app/src/main/java/com/sisfo/practicumfinale/activities/MainActivity.java
+++ app/src/main/java/com/sisfo/practicumfinale/activities/MainActivity.java
@@ -1,16 +1,26 @@
 package com.sisfo.practicumfinale.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.sisfo.practicumfinale.R;
+import com.sisfo.practicumfinale.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            return true;
+        });
     }
 }
